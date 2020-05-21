3948c5722ba874c2b7582d41d764086eea8b0327
Rachel Warbelow <rachelwarbelow@gmail.com>
2020-05-20 23:41:34 -0600

    Adds views and time zone support

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+app.set('view engine', 'pug');
 app.get('/', function (req, res) {
-  var date = new Date()
-  res.send(`The current time is ${date}`);
+  let zone = req.query.zone || 'America/Denver'
+  let date = new Date()
+  let localDate = date.toLocaleString('en-US', { timeZone: zone }).split(' ')[1]
+  
+  // res.send(`<p>The current time in ${zone} is ${localDate}<p><input id='timezone' /><button id='updateZone'>Update Timezone</button>`);
+  // res.render(__dirname + "/views/index.html", {localDate: localDate, zone: zone});
+  res.render('index', { localDate: localDate, zone: zone});
 });
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
